3e1ac5bcbe2565d1da1c27bc45fef35bd9d8e144
seba_sulia <sebas.sulia@gmail.com>
2021-07-08 18:03:38 -0300

    Adding classes

---
+++ Biblioseca.Model/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioseca.Model
+{
+    class Book
+    {
+        public virtual string Title { get; set; }
+        public virtual string Author { get; set; } //ver esto
+        public virtual string Description { get; set; }
+        public virtual string Category { get; set; }
+        public int ISBN { get; set; }
+        public virtual int Id { get; set; }
+
+    }
+}
+
+
